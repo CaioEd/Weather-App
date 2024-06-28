d94d3af41b08987ec66f0c19efb974c3cb5d7da2
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2024-06-28 09:24:55 -0300

    Update JSX

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -2,24 +2,54 @@ import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
-import SearchInput from './components/SearchInput'
-import WeatherInfo from './components/WeatherInfo'
 import WeatherDetails from './components/WeatherDetails'
 
 function App() {
 
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState()
+  const [data, setData] = useState([])
+  const [location, setLocation] = useState('')
 
-  const geocoding = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid={API key}`
+  const geocoding = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=de48ddb9761342006c0215fa0421b9ea`
 
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=de48ddb9761342006c0215fa0421b9ea`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(geocoding)
+        .then((response) => {
+          if (response.data && response.data.length > 0) {
+            setData(response.data);
+            console.log(response.data[0].name);
+          } else {
+            console.error('No data found for the specified location');
+          }
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+      setLocation('');
+    }
+  }
 
   return (
     <>
       <h1>Weather App</h1>
-      <SearchInput/>
-      <WeatherInfo/>
+
+      <input type="search" className='searchInput' name="searchInput" id="searchInput" placeholder='Enter Location' value={location} onChange={event => setLocation(event.target.value)}
+      onKeyDown={searchLocation}/>
+
+      <div className="mainContainer">
+            {data[0] ? (
+              <>
+                <span>{data[0].name}</span>
+              </>
+          ) : (
+            <p></p>
+          )}
+            <h2>18°C</h2>
+            <span>Cloudy</span>
+      </div>
+  
       <WeatherDetails/>
       
     </>
